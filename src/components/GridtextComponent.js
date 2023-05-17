46bd4c07dbf97765bf28465c5cc538309ebfe438
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-05-17 16:53:21 -0700

    gridtext formatting

--- src/components/GridtextComponent.js
+++ src/components/GridtextComponent.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const GridtextComponent = () => {
     return (
-        <article className="p-10">
-            <h3 className='font-semibold text-3xl mb-4'>Transform your brand</h3>
-            <p className='mr-4'>We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
+        <article className="flex flex-col justify-center pl-20 pr-16">
+            <h3 className='font-semibold text-3xl mb-12'>Transform your brand</h3>
+            <p className='items-center mb-4'>We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
         </article>
     );
 };
