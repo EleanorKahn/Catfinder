417ef7864cd4128560ce568e1542a51992299b36
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-07-24 14:27:46 -0700

    positive fetch call using local data

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,111 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import ResultComponent from './ResultComponent';
 
 const SearchComponent = () => {
-    const [submit, setSubmit] = useState(false);
-    const [zipcode, setZipcode] = useState(0);
-    const [age, setAge] = useState("");
-    const [furLength, setFurLength] = useState("");
-    const [goodWith, setGoodWith] = useState("");
-    const [color, setColor] = useState("");
+    const [submit, setSubmit] = useState('false');
+    const [petData, setPetData] = useState({});
+    const [loading, setIsLoading] = useState(false);
 
-
-
-    // useEffect(() => {
-
-    // }, [submit]);
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(zipcode);
-        console.log(color);
-        console.log(age);
-        console.log(goodWith);
-    };
+    useEffect(() => {
+        const petfetch = async () => {
+            try {
+                setIsLoading(true);
+                console.log('I am loading');
+                const url = '/data/data.json';
+                const response = await fetch(url);
+                const data = await response.json();
+                setPetData(data);
+                console.log(petData);
+                setIsLoading(false);
+                console.log('I am not loading anymore')
+            } catch(err) {
+                console.log(err);
+            }
+        }
+        petfetch();
+    },[submit]);
 
     return (
-        <div className='rounded'>
-            <form onSubmit={handleSubmit} className='w-full'>
-                <div className='w-full grid grid-cols-2'>
-                    <div className=''>
-                        <label htmlFor='age'>Age </label>
-                        <select 
-                            id="age"
-                            name="age"
-                            value={age}
-                            onChange={(e) => setAge(e.target.value)}
-                        >
-                            <option>Any</option>
-                            <option>Kitten</option>
-                            <option>Young Adult</option>
-                            <option>Adult</option>
-                            <option>Senior</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor="goodWith">Good with </label>
-                        <select 
-                            className='form-select'
-                            id="goodWith"
-                            name="goodWith"
-                            value={goodWith}
-                            type='select'
-                            onChange={(e) => setGoodWith(e.target.value)}
-                        >
-                            <option>I want to be a solo pet</option>
-                            <option>other cats</option>
-                            <option>other dogs</option>
-                            <option>kids</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor="color">Color </label>
-                        <select
-                            className='form-select'
-                            id="color"
-                            name="color"
-                            value={color}
-                            type="select"
-                            onChange={(e) => setColor(e.target.value)}
-                        >
-                            <option>Any</option>
-                            <option>Black</option>
-                            <option>Tuxedo</option>
-                            <option>White</option>
-                            <option>Tortoishell</option>
-                        </select>
-                        <label htmlFor='furLength'>Fur length </label>
-                        <select
-                            className='form-select'
-                            id="furLenght"
-                            name="furLength"
-                            value={furLength}
-                            type="select"
-                            onChange={(e) => setFurLength(e.target.value)}
-                        >
-                            <option>Any</option>
-                            <option>Hairless</option>
-                            <option>Short</option>
-                            <option>Medium</option>
-                            <option>Long</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor='zipcode'>ZIP Code </label>
-                        <input 
-                            className='form-input'
-                            placeholder='00000'
-                            id="zipcode" 
-                            name="zipcode" 
-                            value={zipcode} 
-                            type="text"
-                            onChange={(e) => setZipcode(e.target.value)}
-                        />
-                    </div>
-                </div>
-                <button type="submit" className='focus:border-red-50 bg-sky-400 text-white max-w-full rounded-sm h-6' onClick={() => setSubmit(true)}>Search</button>
-            </form>
-            <ResultComponent />
+        <div>
+            <button onClick={() => setSubmit(true)}>Find a pet</button>
+            {/* {submit && petData?.map} */}
         </div>
     );
 };
--- src/pages/CatfinderPage.js
+++ src/pages/CatfinderPage.js
@@ -1,54 +1,12 @@
-import { useState, useEffect } from "react";
-import ProtoSearchComponent from "../components/ProtoSearchComponent";
-import ResultComponent from "../components/ResultComponent";
+
+import SearchComponent from "../components/SearchComponent";
+
 
 const CatfinderPage = () => {
-    const [zipcode, setZipcode] = useState("");
-    const [isLoading, setIsLoading] = useState(true);
-    const [data2, setData] = useState([]);
-    const url = "https://api.petfinder.com/v2/oauth2/token"
-    const [go, setGo] = useState(false);
-
-    useEffect(() => {
-        // Call the API
-        // This is a POST request, because we need the API to generate a new token for us
-        fetch(url, {
-            method: 'POST',
-            body: `grant_type=client_credentials&client_id=${process.env.MY_KEY}&client_secret=${process.env.MY_SECRET}`,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        }).then(function (resp) {
-            // Return the response as JSON
-            return resp.json();
-        }).then(function (data) {
-            // Log the API data
-            console.log('token', data);
-            // Return a second API call
-            // This one uses the token we received for authentication
-            return fetch('https://api.petfinder.com/v2/animals?limit=21', {
-                headers: {
-                    'Authorization': data.token_type + ' ' + data.access_token,
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            });
-        }).then(function (resp) {
-            return resp.json();
-        }).then(function (data) {
-            setData(data);
-            console.log('pets', data);
-        }).catch(function (err) {
-            console.log('something went wrong', err);
-        });
-    },[go]);
-    
-
     return (
         <div>
             <h2 className="text-center mb-5 pt-5 font-bold">Catfinder Page</h2>
-            <button className='text-black bg-white' onClick={() => setGo(true)}>Go</button>
-            <ProtoSearchComponent searchZip={(zipcode) => setZipcode(zipcode)} />
-            <ResultComponent data={data2} />
+            <SearchComponent />
         </div>
     );
 };
