83698b57ac4ba8a195eb672d76da352c2d778bc1
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-07-26 15:07:15 -0700

    oauth

--- src/components/CatGridComponent.js
+++ src/components/CatGridComponent.js
@@ -10,7 +10,7 @@ const CatGridComponent = ({ pet }) => {
                 <div>
                     <h3 className='text-center'>{pet.name}</h3>
 
-                    {pet.tags.map((tag, idx) => idx + 1 === pet.tags.length ? <span>{tag}</span> : <span>{tag}, </span>)}
+                    {pet.tags.map((tag, idx) => idx + 1 === pet.tags.length ? <span key={idx}>{tag}</span> : <span key={idx}>{tag}, </span>)}
 
                     <p>View my <a className='text-blue-400' href={pet.url} target='blank'>full profile</a> on Petfinder</p>
                 </div>
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -5,23 +5,52 @@ import Loading from './Loading';
 const SearchComponent = () => {
     const [submit, setSubmit] = useState(false);
     const [petData, setPetData] = useState([]);
+    const [token, setToken] = useState('');
     const [loading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        const petfetch = async () => {
-            try {
-                setIsLoading(true);
-                const url = '/data/data.json';
-                const response = await fetch(url);
-                const data = await response.json();
-                setPetData(data.animals);
-                setIsLoading(false);
-            } catch(err) {
-                console.log(err);
-            }
+    const getToken = async () => {
+        try {
+            setIsLoading(true);
+            // get access token
+            const tokenUrl = 'https://api.petfinder.com/v2/oauth2/token';
+
+            const tokenResponse = await fetch(tokenUrl, {
+                method: 'POST',
+                body: `grant_type=client_credentials&client_id=${process.env.REACT_APP_MY_KEY}&client_secret=${process.env.REACT_APP_MY_SECRET}`,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+
+            const newToken = await tokenResponse.json();
+            setToken(newToken);
+            setIsLoading(false);
+        } catch(err) {
+            console.log(err);
         }
+    }
+
+    const petfetch = async () => {
+        try {
+            setIsLoading(true);
+
+            const findPetUrl = '/data/data.json';
+            const response = await fetch(findPetUrl);
+
+            const data = await response.json();
+            setPetData(data.animals);
+
+            setIsLoading(false);
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        getToken();
         petfetch();
     }, [submit]);
+
 
     return (
         <div>
