b833076210fbf08c29b16eee1118351ee37bac2b
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-07-25 17:25:41 -0700

    name, tags, petfinder link

--- src/components/CatGridComponent.js
+++ src/components/CatGridComponent.js
@@ -3,8 +3,13 @@ import React from 'react';
 const CatGridComponent = ({ pet }) => {
     console.log(pet.name);
     return (
-        <div>
-            <p>Hi, I am a cat</p>
+        <div className='shadow-md border-2 mb-3'>
+            <div className='bg-white'>
+                <h3>{pet.name}</h3>
+                {pet.tags.map((tag) => <span>{tag}, </span>)}
+            </div>
+            <a href={pet.url} target='blank'>View my full profile on Petfinder</a>
+            
         </div>
     );
 };
